1a4474d5b1c6a243d125c7f695a7cfb3ca2a6256
Rama Krishna <ramakrishnaprasadvuppala@gmail.com>
2022-09-08 01:49:36 -0700

    printing first message using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * Header file for puts function
+ * Main entry point
+ * usage of puts function
+ * exit
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
