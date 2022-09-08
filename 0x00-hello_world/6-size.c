a5f53fa58a69bf21467dfbb108d24b757efbaf32
Rama Krishna <ramakrishnaprasadvuppala@gmail.com>
2022-09-08 02:48:18 -0700

    program for finding different types bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,7 +11,7 @@ int main(void)
 	int b;
 	long int c;
 	long long int d;
-	float c;
+	float e;
 	printf("Size of a char: %lu byte(s)\nSize of an int: %lu byte(s)\nSize of a long int: %lu byte(s)\nSize of a long long int: %lu byte(s)\nSize of a float: %lu byte(s)",sizeof(a),sizeof(b),sizeof(c),sizrof(d),sizeof(e));
 	return (0);
 }
