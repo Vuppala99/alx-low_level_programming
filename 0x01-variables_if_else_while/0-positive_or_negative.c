fbe747381d323f8c1387bdc13e29d596a2c5ea8b
Rama Krishna <ramakrishnaprasadvuppala@gmail.com>
2022-09-09 04:55:23 -0700

    usage of if else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-
 /**
  * main - Entry point
  *
@@ -13,7 +12,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0)
