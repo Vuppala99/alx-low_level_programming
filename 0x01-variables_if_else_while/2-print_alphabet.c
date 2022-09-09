be8295d23de3703ba5a414523be3101007651459
Rama Krishna <ramakrishnaprasadvuppala@gmail.com>
2022-09-09 06:22:16 -0700

    print lower alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -10,10 +9,7 @@ int main(void)
 	char ch;
 
 	for (ch = 'a' ; ch <= 'z' ; ch++)
-	{
 		putchar(ch);
-		putchar('\n');
-	}
-
+	putchar('\n');
 	return (0);
 }
