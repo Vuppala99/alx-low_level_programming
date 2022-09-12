c403ffb5247ffe85dbfe4ce1983e55c776bbcce5
Rama Krishna <ramakrishnaprasadvuppala@gmail.com>
2022-09-11 19:41:08 -0700

    usage of nested for

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,16 +10,19 @@ int main(void)
 
 	for (i = '0'; i <= '8'; i++)
 	{
-		for (j = i + 1; j <= 9 ; j++)
+		for (j = i + 1; j <= '9' ; j++)
 		{
+			if (i != j)
+			{
 			putchar(i);
 			putchar(j);
-			if ((i != '8') | (j != '9'))
-			{
-				putchar(',');
-				putchar(' ');
-			}
+
+			if (i == '8' && i == '9')
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
+	}
 	}
 	putchar('\n');
 
