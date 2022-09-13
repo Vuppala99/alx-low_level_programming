73ff3e8b8f6ff57d9423f18cf7781d012927eecd
Rama Krishna <ramakrishnaprasadvuppala@gmail.com>
2022-09-13 06:48:24 -0700

    Function to add 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - adding 2 numbers
+ * @a: a is  passed as first parameter
+ * @b: b is passed as second parameter
+ *
+ * Return: integer
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
