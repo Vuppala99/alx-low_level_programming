250f0d0494826cf1bd84e45b5b41db000dfa3d62
Rama Krishna <ramakrishnaprasadvuppala@gmail.com>
2022-09-13 03:30:32 -0700

    Function to check sign of number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,11 +10,20 @@ int print_sign(int n)
 	int print_sign;
 
 	if (n > 0)
+	{
 		print_sign = 1;
+		_putchar('+');
+	}
 	else if (n == 0)
+	{
 		print_sign = 0;
+		_putchar('0');
+	}
 	else
+	{
 		print_sign = -1;
+		_putchar('-');
+	}
 
 	return (print_sign);
 }
