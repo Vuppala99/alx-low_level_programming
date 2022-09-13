0fb1f3a85dee140e201ca71d89325b4bd01f970f
Rama Krishna <ramakrishnaprasadvuppala@gmail.com>
2022-09-13 03:47:33 -0700

    Function for printing absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * _abs - prints absolute value
+ *@i: i is passed as argument
+ *
+ * Return: integer
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+		return (i);
+	else
+		return (i * -1);
+}
