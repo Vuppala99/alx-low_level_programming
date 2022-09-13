50f4e010687fd65626e2da9e8461d19a78f63f8d
Rama Krishna <ramakrishnaprasadvuppala@gmail.com>
2022-09-13 03:24:57 -0700

    function for checking sign of number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_sign - check sign of number
+ * @n: n is passed as argument
+ *
+ * Return: 1 if n greater than 0 or 0 if n is zero or -1 if nis less than 0
+ */
+int print_sign(int n)
+{
+	int print_sign;
+
+	if (n > 0)
+		print_sign = 1;
+	else if (n == 0)
+		print_sign = 0;
+	else
+		print_sign = -1;
+
+	return (print_sign);
+}
+
