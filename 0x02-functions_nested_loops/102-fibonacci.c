1bcc8cb771b561a61175c7b60d94dc15e34c6ccc
Rama Krishna <ramakrishnaprasadvuppala@gmail.com>
2022-09-13 18:39:59 -0700

    print first 50 fibonocci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	unsigned long i, a = 0, b = 1, add;
+
+	for (i = 0; i < 50; i++)
+	{
+		add = a + b;
+		printf("%lu", add);
+		a = b;
+		b = add;
+		if (i == 49)
+			continue;
+		printf(", ");
+	}
+	printf("\n");
+}
+
