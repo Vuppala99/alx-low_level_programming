99a42b7eedb871bfae131dde1919a408df29c667
Rama Krishna <ramakrishnaprasadvuppala@gmail.com>
2022-09-15 04:40:35 -0700

    print triangle based on n

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_triangle - print triangle
+ * @size: size is given as argument
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			j = i;
+			for (; j < (size - 1); j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(35);
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
