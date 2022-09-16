ddce1a475c1fe4c88ac406c638fa18bab6f0ee1c
Rama Krishna <ramakrishnaprasadvuppala@gmail.com>
2022-09-15 19:25:20 -0700

    Finding largest prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,13 +6,18 @@
  */
 int main(void)
 {
-	unsigned long int i;
+	unsigned long int i = 2, k = 0;
 	unsigned long int n = 612852475143;
 
-	for (i = 3; i < 12057; i += 2)
+	while (n > i)
 	{
-		while ((n % i == 0) && n != i)
-			n /= n / i;
+		while (n % i == 0)
+		{
+			if (i > k)
+				k = i;
+		n = n / i;
+		}
+		i++;
 	}
-	printf("%lu\n", n);
+	printf("%lu\n", k);
 }
