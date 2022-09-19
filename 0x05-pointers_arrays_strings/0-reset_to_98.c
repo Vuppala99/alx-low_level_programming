447901099545076cfab86419b47c8763ee8b5856
Rama Krishna <ramakrishnaprasadvuppala@gmail.com>
2022-09-18 21:34:24 -0700

    assigning value for n using pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - updating value of pointer pointing to 98
+ * @n: n is a pointer
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+using arrays pointers and strings
