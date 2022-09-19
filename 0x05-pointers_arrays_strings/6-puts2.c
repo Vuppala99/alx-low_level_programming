bfb62ca9c411ef24cf9e66f84ce765efbc670290
Rama Krishna <ramakrishnaprasadvuppala@gmail.com>
2022-09-19 04:19:05 -0700

    prints alternate string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,31 @@
 #include "main.h"
 /**
+ * _strlen - prints lenght of a string
+ * @s : passed as string
+ *
+ * Return: lenght
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+/**
  * puts2 - prints alternate charecter
+ * @str: passed as parameter
  *
  * Return: void
  */
 void puts2(char *str)
 {
-	int i;
+	int i, len;
 
+	len = _strlen(str) - 1;
 	i = 0;
-	while (str[i] != '\0')
+	while (i <= len)
 	{
 		_putchar(str[i]);
 		i += 2;
